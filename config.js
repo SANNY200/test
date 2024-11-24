39796a783e1e72b379c60e52f4d29152e9c43fd5
SANNY200 <sannyofficial2001@gmail.com>
2024-11-25 01:01:06 +0530

    Update config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MONGODB_URI: 'mongodb://mongo:tLciqKGwcrLQvQvdkwavwoGPETVxyInA@junction.proxy.rlwy.net:16559', // MongoDB connection URI
+  PREFIX: '.', // Command prefix for the bot
+  ALIVE_IMG: 'https://api.logo.com/api/v2/images?logo=lg_kLwYGiGe3w59sFT356&width=128&height=128&fit=contain&margins=24&format=webp&quality=60&u=1732406547590', // Link to the image displayed when bot is alive
+  AUTO_READ_STATUS: 'true', // Whether to automatically read status messages
+  SESSION_ID: '0sp0EBSY#IEvQbsNB8eSHy4nfwyGnfLpZEvHGnwbLfS0bGwtxTak', // Session ID for Baileys
+  BOT_NUMBER: '947060754447', // Your WhatsApp bot number
+};
